b293260be1e27979e2cf11c8db4532dc407214dd
elmoushy <seif778811@gmail.com>
2025-08-12 06:10:14 +0300

    remove

--- authentication/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Authentication package for Azure AD integration.
-
-This package provides JWT-based authentication using Azure AD (Microsoft Entra ID)
-for the WeaponPowerCloud backend API.
-"""
